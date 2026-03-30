89381c5409b67d55bbd1ae0a5def3a13ba5cd5e2
Rodrigo Couto <r@rodg.co>
2026-03-30 05:38:06 -0300

    release: v0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RodarFeel.MixProject do
   def project do
     [
       app: :rodar_feel,
-      version: "0.2.2-dev.1",
+      version: "0.2.2",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       package: package(),
